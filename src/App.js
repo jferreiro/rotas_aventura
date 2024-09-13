487b0c4a979e08aa903f866c313622ce2e007596
Joao Ferreiro <joao.ferreiro@gmail.com>
2024-09-13 20:27:48 +0400

    Change routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import TourForm from './TourForm';
 import LandingPage from './LandingPage'; // Optional placeholder
 
